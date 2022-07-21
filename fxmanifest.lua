6ba439a17d7ff5f6c8c8509cdaa1a39ef97dce4b
稚翔 曾 <sean920310@gmail.com>
2022-07-22 03:27:28 +0800

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+
+game "gta5"
+
+server_scripts {
+    "@mysql-async/lib/MySQL.lua",
+    "config.lua",
+    "server.lua",
+}
+
+client_scripts {
+    "config.lua",
+    "client.lua",
+}
+
+-- ui_page "html/index.html"
+
+-- files {
+--     'html/index.html',
+--     'html/index.js',
+--     'html/index.css',
+--     'html/reset.css',
+--     'html/asset/img/*.png'
+-- }
